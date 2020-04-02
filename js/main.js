af8f77753afc47c210e260ccfd46b79e29e70d45
azaelsanchez <cem20619@gmail.com>
2020-04-02 11:10:30 +0200

    Reloj implementado con momentjs

--- js/main.js
+++ js/main.js
@@ -102,4 +102,11 @@ $(document).ready(function(){
       if(window.location.href.indexOf('about')> -1){
         $('#acordeon').accordion();
       }
+      if(window.location.href.indexOf('reloj')> -1){
+        setInterval(function(){
+
+          var reloj = moment().format("hh:mm:ss");
+          $('#reloj').html(reloj);
+        },1000);
+      }
 });
